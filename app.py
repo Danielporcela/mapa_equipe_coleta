312b0ef044838736d3005e01d6f0513dd05b93a5
Seu Nome <daniel993200168@gmail.com>
2026-05-07 17:50:37 -0300

    Projeto pronto para render

--- app.py
+++ app.py
@@ -496,4 +496,5 @@ def cadastrar_status():
 
 # ================= START =================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
